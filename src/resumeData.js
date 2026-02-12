d9e6c3051945ae3d81b9a21b13e9fb8c16508716
David Bootle <davidtbootle@gmail.com>
2026-02-12 15:40:18 -0500

    Added the rest of my experience

--- src/resumeData.js
+++ src/resumeData.js
@@ -25,13 +25,70 @@ const resumeData = {
     ],
     experience: [
         {
-            name: 'Bootle Tech',
-            position: 'Owner/Founder',
-            location: 'Charleston, SC',
-            timeRange: '2020-2022',
+            name: 'NAVSEA Naval Surface Warfare Center',
+            position: 'Developer Intern',
+            location: 'Dahlgren, VA',
+            timeRange: 'Jun 2025 - Aug 2025',
             description: [
-                "Founded a tech support venture serving individuals with troubleshooting and installation needs.",
-                "Hosted free monthly Q&A sessions at the senior center, offering complimentary on-site support for attendees."
+                "Developed front end and back end features using Angular, TypeScript, C#, and Elasticsearch.",
+                "Collaborated closely with a team of developers in an agile environment, contributing to sprint planning, code reviews, and integration testing to ensure high-quality, maintainable software.",
+                "Enhanced team productivity by bringing my experience to existing problems, creating modular reusable components, and refactoring inefficient or unneeded code."
+            ]
+        },
+        {
+            name: 'Clemson College of Engineering, Computing, and Applied Sciences',
+            position: 'ECEntials Lab Manager',
+            location: 'Clemson, SC',
+            timeRange: 'Aug 2023 - Dec 2024',
+            description: [
+                "Managed inventory, maintenance, and operations for a 3D printing farm of five Bambu Labs X1C printers.",
+                "Designed and updated automation and scheduling programs for ease of use by lab operators and students.",
+                "Trained six lab operators to operate print farm and manage soldering stations."
+            ]
+        },
+        {
+            name: 'MyOverlap',
+            position: 'Front End Lead',
+            timeRange: 'May 2023 - Aug 2024',
+            location: 'Clemson, SC',
+            description: [
+                "Cofounded intuitive group scheduling app myOverlap.",
+                "Programmed the front end for myoverlap.com in React and Sass. Designed complex and unique interface elements that elevate the user experience.",
+                "Worked with small team to rapidly innovate with new features and user experiences.",
+                "Collected feedback from users and worked to rapidly apply good suggestions to improve the site."
+            ]
+        },
+        {
+            name: 'Clemson Univeristy Bioengineering',
+            position: 'Lab Developer',
+            timeRange: '2022-2024',
+            location: 'Clemson, SC',
+            description: [
+                "Collaborating with researchers to craft hardware and software enhancing data collection and processing using MATLAB, Python, Simulink, and Arduino.",
+                "Created Advanced Reactor Control via OPC in Python (ARCOpy) to interface with AMBR robotic system, allowing for previously impossible experiments and automation."
+            ]
+        },
+        {
+            name: 'Ref4Me',
+            position: 'Testing Developer',
+            location: 'San Diego, CA',
+            timeRange: 'May 2022 - Aug 2022',
+            description: [
+                "Led the migration of the web app to Docker, ensuring system reliability.",
+                "Developed the application using Python Flask and PyCharm.",
+                "Authored Selenium test suites with Behave for automated testing.",
+                "Collaborated with developers and management to achieve timely feature deployment."
+            ]
+        },
+        {
+            name: 'JMS Media LLC',
+            position: 'App Developer',
+            location: 'Hanahan, SC',
+            timeRange: 'Aug 2021 - Oct 2021',
+            description: [
+                "Engineered the UI and notification system for BITAC Mobile Connect app using Flutter and Dart.",
+                "Integrated push notifications with Firebase Cloud Messaging.",
+                "Seamlessly collaborated on the integration of front-end and back-end functionalities,including database and API interactions."
             ]
         },
         {
@@ -43,6 +100,16 @@ const resumeData = {
                 "Engaged directly with clients, handling hardware installations and software troubleshooting.",
                 "Safely transported and set up advanced technology equipment for educational institutions and businesses.",
                 "Demonstrated professionalism and independence, empowering the company to expand its client base."
+            ]
+        },
+        {
+            name: 'Bootle Tech',
+            position: 'Owner/Founder',
+            location: 'Charleston, SC',
+            timeRange: '2020-2022',
+            description: [
+                "Founded a tech support venture serving individuals with troubleshooting and installation needs.",
+                "Hosted free monthly Q&A sessions at the senior center, offering complimentary on-site support for attendees."
             ]
         }
     ]
