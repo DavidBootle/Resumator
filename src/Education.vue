fb376716b2c1a679665332dfea4f50cd9464db8d
David Bootle <davidtbootle@gmail.com>
2026-02-12 14:58:24 -0500

    Added description support for education fields

--- src/Education.vue
+++ src/Education.vue
@@ -3,18 +3,23 @@
     defineProps(['data']);
 
     let isShown = ref(true);
+    let isDescriptionShown = ref(true);
 </script>
 
 <template>
     <div v-if="isShown" @click="isShown = false;" class="education-container">
-        <div class="left col">
-            <div><span class="bold-text">{{ data.name }}</span> (GPA: {{ data.gpa }})</div>
-            <i class="normal-text">{{ data.degree }}</i>
+        <div class="education-header-row">
+            <div class="left col">
+                <div><span class="bold-text">{{ data.name }}</span> (GPA: {{ data.gpa }})</div>
+                <i class="normal-text">{{ data.degree }}</i>
+            </div>
+            <div class="right col">
+                <div class="normal-text">{{ data.location }}</div>
+                <div class="normal-text">{{ data.timeRange }}</div>
+            </div>
         </div>
-        <div class="right col">
-            <div class="normal-text">{{ data.location }}</div>
-            <div class="normal-text">{{ data.timeRange }}</div>
-            
+        <div v-if="isDescriptionShown && data.description" @click.stop="isDescriptionShown = false;" class="education-description">
+            {{ data.description }}
         </div>
     </div>
 </template>
@@ -23,14 +28,18 @@
 
 .education-container {
     margin-top: 8px;
-    display: flex;
-    flex-direction: row;
-    justify-content: start;
     padding-left: 18px;
     width: calc(100%-18px);
     font-family: 'Oswald';
     font-size: 16px;
     font-weight: 300;
+}
+
+.education-header-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    width: 100%;
 }
 
 .col {
